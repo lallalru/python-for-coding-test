01965443a02461718e262f080c0d30517725d238
lallalru <>
2021-05-28 06:58:54 +0000

    Add A01 code - Need again try!

---
+++ Part3/A01.py
@@ -0,0 +1,14 @@
+n = input()
+m = list(map(int, input().split()))
+m.sort()
+
+#count : 여행에 참여하는 인원 수
+count = result = 0
+
+for i in m:
+  count +=1
+  if count >= i:
+    result += 1
+    count = 0
+
+print(result)
