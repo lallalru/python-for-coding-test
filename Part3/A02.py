6037a29540691c5b8d26536a4cabd28df3d49b7a
lallalru <>
2021-05-28 05:36:17 +0000

    Add A02 code

---
+++ Part3/A02.py
@@ -0,0 +1,12 @@
+s = input()
+result = int(s[0])
+
+for i in range(1,len(s)):
+  if result == 0:
+    result += int(s[i])
+  elif s[i]== '0' or s[i] == '1' :
+    result += int(s[i])
+  else:
+    result *= int(s[i])
+
+print(result)
